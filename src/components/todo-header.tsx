6540522b069c1a93ad3ca6a7caa160ccbc822d0e
Serge Semenidov <23sergey03@gmail.com>
2026-09-19 18:39:03 +0300

    broke

--- src/components/todo-header.tsx
+++ src/components/todo-header.tsx
@@ -26,7 +26,7 @@ export function TodoHeader({ completedCount, totalCount }: TodoHeaderProps) {
                 </h1>
                 {totalCount > 0 && (
                     <Badge variant="secondary" className="tabular-nums">
-                        {completedCount} / {totalCount} done
+                        {completedCount} / {totalCount} don
                     </Badge>
                 )}
             </div>
